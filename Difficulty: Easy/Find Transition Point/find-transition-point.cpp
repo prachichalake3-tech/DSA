c584344d9d9b5b71af27c7daf3b651d652db65ad
prachichalake3-tech <prachichalake3@gmail.com>
2026-07-31 00:09:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Transition Point/find-transition-point.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int transitionPoint(vector<int>& arr) {
+        // code here
+        for(int i=0; i<arr.size();i++){
+            if(arr[i]==1)
+            return i;
+        }
+        return -1;
+    }
+};
