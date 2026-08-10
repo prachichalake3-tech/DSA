7f6d4455c4723f15b0ac71c28939723d7363e70e
prachichalake3-tech <prachichalake3@gmail.com>
2026-08-10 11:44:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/First 1 in a Sorted Binary Array/first-1-in-a-sorted-binary-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstIndex(vector<int>& arr) {
+        int low = 0;
+        int high = arr.size() - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == 1) {
+                ans = mid;
+                high = mid - 1;   
+            }
+            else {
+                low = mid + 1;    
+            }
+        }
+
+        return ans;
+    }
+};
