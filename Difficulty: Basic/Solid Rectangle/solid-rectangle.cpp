e354972d2bad2bf6098435f6fc77077e93e778b3
prachichalake3-tech <prachichalake3@gmail.com>
2026-08-27 08:39:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Solid Rectangle/solid-rectangle.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    // code here
+
+      int i, j;
+
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= m; j++) {
+                cout << "* ";
+            }
+            cout << endl;
+        }
+
+      
+    return 0;
+}
