bb20839c9440cca895c4f7bf71803ace30e18ec7
prachichalake3-tech <prachichalake3@gmail.com>
2026-09-16 17:51:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Peak element/peak-element.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int peakElement(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+
+        if (n == 1)
+            return 0;
+
+        if (arr[0] > arr[1])
+            return 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1])
+                return i;
+        }
+
+        if (arr[n - 1] > arr[n - 2])
+            return n - 1;
+
+        return -1;
+    
+
+    }
+};
